1ac8828b3079132a568388b7789987c964d32f4a
AhmedNoureldinAhmed <m-32500912@s-mu.edu.eg>
2019-10-26 18:40:12 +0200

    Add File System

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include<vector>
+#include<fstream>
 #include<algorithm> //log10()
 #include<cmath>     //Max,Min in Vector
 #include<iomanip>   //setw()
@@ -11,18 +12,23 @@ using namespace std;
 int main()
 {
 
-  vector<double>_data
-  { 22 ,37 ,28, 63, 32, 26 ,31, 27, 27, 28,
-    30 ,26 ,29 ,24 ,38 ,25 ,29 ,41, 30 ,35,
-    35 ,33 ,29 ,38 ,54 ,24 ,25 ,46 ,41 ,28,
-    40 ,39 ,29 ,27 ,31 ,38 ,29 ,25 ,35 ,60,
-    43 ,35 ,34 ,34 ,27 ,37 ,42 ,41 ,36 ,32,
-    41 ,33 ,31 ,74 ,33 ,50 ,38 ,61 ,21 ,41,
-    26 ,80 ,42 ,29 ,33 ,35 ,45 ,49 ,39 ,34,
-    26 ,25 ,33 ,35 ,35 ,28
-    }
-,_lower,_upper;
-double _max{},_min{},_sum{};
+vector<double>_data,_lower,_upper;
+double _max{},_min{},_re{};
+
+// =========== FileHandling Read =========== //
+ifstream input;;
+ input.open("input.txt",ios::in | ios::app);
+
+ if (input.fail())
+    cout<<"Error in input File"<<endl;
+ input.seekg(168, ios::beg); // Skip Positions (Text in Begin File)
+char sign{};
+ while(input){
+    input>>_re; // Read Number
+    input>>sign; // Read Colon
+    _data.push_back(_re); // Assigned Data To Vector
+}
+// =========== FileHandling =========== //
 
  // ==============  Sturges rule  ============== //
  const size_t _n{_data.size()}; //Number of Data in Vector
@@ -32,46 +38,40 @@ double _max{},_min{},_sum{};
  const double _classW{ceil(_range / _k)};// Calculate Class Width
  //double _classW{round(_range / 6)} // Another Method ;
 
-
-// ============== Table Header ============== //
-    cout<<"-------------------------"<<endl;
-    cout<<"Class Limit "<<setw(12)<<"Frequency"<<endl;
-    cout<<"-------------------------"<<endl;
-
 // ============== Assigned Class Limit ============== //
-    for(size_t i=0; i <_k;i++)
-    {
-        if( i==0 )
-        {
-         _lower.push_back(*min);
-         _upper.push_back(*min + (_classW -1) );
-        }
-        // Assigned Value
+ for(size_t i=0; i <_k;i++)
+ {
+    if( i==0 )
+    {    _lower.push_back(*min);
+         _upper.push_back(*min + (_classW -1) );}
+  // Assigned Lower & Upper Data
     _min+=_classW ; //
     _max =_min + (_classW -1) ;
     _lower.push_back(_min);
     _upper.push_back(_max);
  }
-
-
-// ============== Counter ============== //
+// ============== Counter & Sum ============== //
 vector<double>_count(_k);
+double _sum{};
 
-    for(size_t i=0; i <_data.size();i++)
+ for(size_t i=0; i <_data.size();i++)
+ {
+    for (size_t j=0 ; j < _k ;j++)
     {
-        for (size_t j=0 ; j < _k ;j++)
+        if( _data[i] >= _lower[j] &&  _data[i] <= _upper[j]  )
         {
-            if( _data[i] >= _lower[j] &&  _data[i] <= _upper[j]  )
-            {
-              _count[j] += 1;
-            }
-
+            _count[j] += 1;
         }
-     }
-    for(size_t i=0 ; i<_count.size();i++)
-        _sum+=_count[i];
+    }
+ }
+ for(size_t i=0 ; i<_count.size();i++)
+    _sum+=_count[i];
 
 // ============== Print ============== //
+    cout<<"Frequency Distribution Table"<<endl;
+    cout<<"-------------------------"<<endl;
+    cout<<"Class Limit "<<setw(12)<<"Frequency"<<endl;
+    cout<<"-------------------------"<<endl;
     for (size_t i=0 ; i < _count.size();i++)
     {
     cout<<"|"<<setw(4)
@@ -88,6 +88,33 @@ vector<double>_count(_k);
  cout<<"-------------------------"<<endl;
 
 // ============== Print ============== //
+
+// =========== FileHandling OutPut =========== //
+    ofstream Freq;
+    Freq.open("output.txt",ios::out  | ios::app);
+
+    if (Freq.fail())
+    cout<<"Error in output File"<<endl;
+
+    Freq<<"Frequency Distribution Table"<<endl;
+    Freq<<"-------------------------"<<endl;
+    Freq<<"Class Limit "<<setw(12)<<"Frequency"<<endl;
+    Freq<<"-------------------------"<<endl;
+    for (size_t i=0 ; i < _count.size();i++)
+    {
+    Freq<<"|"<<setw(4)
+    <<_lower[i]<<"-"<<_upper[i]
+    <<setw(3)<<"|"<<setw(10)
+    <<_count[i]
+    <<setw(4)<<"|"<<endl;
+    }
+     Freq<<"-------------------------"<<endl;
+     Freq<<setw(9)<<" Total "<<
+     setw(2)<<"|"<<
+     setw(10)<<_sum<<
+     setw(4)<<"|"<<endl;
+ Freq<<"-------------------------"<<endl;
+ // =========== FileHandling =========== //
 
 
     return 0;
